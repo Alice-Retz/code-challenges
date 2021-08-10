e0f4eaf2fed7c26e5fb492861ab5f50e9722cb19
Alice-Retz <alicegretz@gmail.com>
2021-08-10 13:14:06 -0500

    First 5 problems solved

---
+++ src/codeChallenge1.js
@@ -0,0 +1,34 @@
+export function doubleNumbers(arr) {
+    return arr.map((num) => num * 2);
+}
+
+export function stringItUp(arr) {
+    return arr.map((num) => {
+        const string = num + '';
+        return string;
+    });
+}
+
+export function capitalizeNames(arr) {
+    return arr.map((item) => {
+        const nameDown = item.toLowerCase();
+        const nameUp = nameDown.charAt(0).toUpperCase() + nameDown.slice(1);
+        return nameUp;
+    } )
+}
+
+export function namesOnly(arr) {
+    return arr.map((item) => {
+        return item.name;
+    })
+}
+
+export function makeStrings(arr) {
+    return arr.map((item) =>{
+        if ( item.age >= 18) {
+            return `${item.name} can go to The Matrix`;
+        } else {
+            return `${item.name} is under age!!`;
+        }
+    })
+}
